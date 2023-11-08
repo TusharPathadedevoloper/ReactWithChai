11b370bf68bd190192d60147d4ccc853120da805
TusharPathadedevoloper <tusharpathade32@gmail.com>
2023-11-08 12:40:10 +0530

    all not good

--- 03tailwindPropsReact/src/App.jsx
+++ 03tailwindPropsReact/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
    <div className="w-full h-screen duration-500" style={{backgroundColor:color}}>
     <div className="fixed  flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-      <div className="flex flex-wrap gap-3 justify-center shadow-lg bg-white rounded-lg py-3 px-3">
+      <div className="flex flex-wrap gap-3 justify-center shadow-lg bg-white rounded-lg py-3">
         <button onClick={()=>setColor("red")}
         className="outline-none px-4  py-1  rounded-full  text-white shadow-lg"
          style={{backgroundColor:"red"}}>
